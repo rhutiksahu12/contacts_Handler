730eda1a39698e5b2b039759bbd8e7146a99188a
Rhutik Sahu <shubhamsahu938@gmail.com>
2023-04-18 16:58:13 +0200

    readme added

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+
+<p>This application is build Vite, ReactJS & TailwindCSS</p>
+
+# To Start the Applicaioon
+ 
+- <em> npm i </em>
+- <em> npm run dev </em>
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -6,7 +6,7 @@ const SearchBar = ({ search, setSearch }) => {
     return (
         <div className=" mt-10 ">
 
-            <input className="float-right px-5 mx-5 rounded-sm shadow bg-inherit text-white placeholder:text-white" type="text" placeholder="Search User" value={search} onChange={(e) => {
+            <input className="float-right px-5 mx-5 rounded-sm shadow bg-inherit text-white md:placeholder:text-white" type="text" placeholder="Search User" value={search} onChange={(e) => {
                 setSearch(e.target.value);
 
             }} />
